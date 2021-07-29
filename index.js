beb161405624e5224477dc494d32e32b73f49e18
unknown <boypham1234567@gmail.com>
2021-07-29 18:34:23 +0700

    test

--- index.js
+++ index.js
@@ -59,22 +59,25 @@ const admins = [
 ];
 
 //----------------------------------CORS------------------------------------
-app.use(cors());
-app.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "https://hrmapplication.herokuapp.com/");
-  // res.header("Access-Control-Allow-Credentials", "true")
-  // res.header("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
-});
+app.use(cors({
+  origin : "*",
+  methods : ["POST", "GET", 'PUT', "DELETE"]
+}));
+// app.use((req, res, next) => {
+//   // res.header("Access-Control-Allow-Origin", "https://hrmapplication.herokuapp.com/");
+//   // res.header("Access-Control-Allow-Credentials", "true")
+//   // res.header("Access-Control-Allow-Headers", "*");
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, DELETE, OPTIONS"
+//   );
+//   next();
+// });
 
 //----------------------------------ADMIN CONTROLLER------------------------------------
 // const loginAdmin = require("./app/controller/loginAdmin")
