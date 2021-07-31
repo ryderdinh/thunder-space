49d37ca439297c06ebd077d84cb4717e6da5da1c
unknown <boypham1234567@gmail.com>
2021-08-01 01:49:06 +0700

    fix create project

--- app/controller/users/project/apiPostProject.js
+++ app/controller/users/project/apiPostProject.js
@@ -3,8 +3,7 @@ const Staff = require("../../../models/staffInformation")
 const router = require("express").Router()
 const authenticateToken = require("../../../../middleware/user/login/authenticateToken")
 const { v4: uuidv4 } = require('uuid');
-const { findById } = require("../../../models/staffInformation");
-const { set } = require("mongoose");
+
 router.post("/createProject/:id", async (req, res, next) => {
     try {
         const userId = req.params.id
@@ -15,80 +14,90 @@ router.post("/createProject/:id", async (req, res, next) => {
         const name = req.body.projectName
         var manager = [... new Set(req.body.projectManager.map(e => e = e.email))]
         var member = [... new Set(req.body.projectMember.map(e => e = e.email))]
-        const existCreater = await Staff.findById(userId)
-        // console.log(existCreater);
-        if(!existCreater){
+        //Valid creater
+
+        var existCreater = await Staff.findById(userId)
+        if (!existCreater) {
             return res.json({
                 data: {
-                    status: "You are not access to project !"
+                    status: "You can not create to project !"
                 }
             })
-        }else{
-        const tempManager =  manager.findIndex( e => e === existCreater.email)
-          if (tempManager < 0 ) {
-              manager.push(existCreater.email)
-          } 
-          const tempMember = member.findIndex( e => e === existCreater.email)
-          if ( tempMember  >= 0 ) {
-              member.splice(tempMember, 1)
-          } 
+        } else{
+            if(manager.find(e => e === existCreater.email) == undefined && member.find(e => e === existCreater.email) == undefined){
+                manager.push(existCreater.email)
+                member.push(existCreater.email)
+            }
         }
-
         const existCode = await Project.findOne({
             code: code
         })
-        const existManager = await Staff.find({
+        var existManager = await Staff.find({
             email: manager
         })
-        // console.log(manager);
-        const existMember = await Staff.find({
-            email : member
+        // console.log(existManager);
+        var existMember = await Staff.find({
+            email: member
         })
+        // console.log(existCreater);
+          
+        //Valid code 
         if (existCode) {
-           return res.json({
+            return res.json({
                 data: {
                     status: "Code has been created !"
                 }
             })
         }
-        // console.log(existManager);
-        if(existManager.length >= 1){
-                for (let i =0; i < existManager.length; i++){
-                        existManager[i] = {
-                            uid : existManager[i].id,
-                            name : existManager[i].name
-                        }
+        //Valid exsit member
+        if (existManager.length > 0 && existMember.length > 0) {
+            //Check data 
+            for(let i =0 ; i < manager.length; i++){
+               let check = member.find(e => e === manager[i])
+               if(check == undefined) return res.json({
+                data: {
+                    status: "Wrong data request !"
                 }
-                manager = existManager
-        }
-        if(existMember.length > 0){
-            for (let i =0; i < existMember.length; i++){
+            })
+            }
+            //Map manager data
+            for (let i = 0; i < existManager.length; i++) {
+                existManager[i] = {
+                    uid: existManager[i].id,
+                    name: existManager[i].name
+                }
+            }
+            //Map  member data
+            for (let i = 0; i < existMember.length; i++) {
                 existMember[i] = {
-                    uid : existMember[i].id,
-                    name : existMember[i].name
+                    uid: existMember[i].id,
+                    name: existMember[i].name
                 }
+            }
+
         }
-        member = existMember.concat(manager)
-        }else{
-        member = existMember.concat(manager)
-        }
-      const result =  await Project.create({
-          pid : pid,
-          code : code,
-          name : name,
-          manager : manager,
-          member : member
-      })
-      result.save()
-     return res.json({data : {
-        status : "Create project successfully !"
-    }})
-    } catch (err) {
-        if(err){
+        //Create project
+        const result = await Project.create({
+            pid: pid,
+            code: code,
+            name: name,
+            manager: existManager,
+            member: existMember
+        })
+        result.save()
+        return res.json({
+            data: {
+                status: "Create project successfully !"
+            }
+        })
+    }catch(err) {
+        if (err) {
             console.log(err);
-           return res.json({data : {
-                status : "Something went wrong !"
-            }})
+            return res.json({
+                data: {
+                    status: "Something went wrong !"
+                }
+            })
         }
     }
 })
