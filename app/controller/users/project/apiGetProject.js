1cf33153e4656cccbe0a18860ec92d9e38d37b8a
unknown <boypham1234567@gmail.com>
2021-08-02 22:26:32 +0700

    search

--- app/controller/users/project/apiGetProject.js
+++ app/controller/users/project/apiGetProject.js
@@ -13,18 +13,25 @@ router.get("/projectInfo/:id", async (req, res, next) => {
         var existProject
         var existMember
         var temp
-        if( search === "member" && code!= undefined ){
+        if( search === "member" && code != undefined ){
             existMember = await Project.find({
                 code : code,
                 member  : { $elemMatch : { uid : uid } }
             })
-           for(let i=0; i< existMember[0].member.length ; i++){
-               existMember[0].member[i] = {
-                   uid : existMember[0].member[i].uid,
-                   name : existMember[0].member[i].name
-               }
-           }
-           return res.json(existMember[0].member)
+          
+            if(existMember.length > 0){
+                for(let i=0; i< existMember[0].member.length ; i++){
+                    existMember[0].member[i] = {
+                        uid : existMember[0].member[i].uid,
+                        name : existMember[0].member[i].name
+                    }
+                }
+                return res.json(existMember[0].member)
+            }else{
+                return res.json({data : {
+                    status : `You are not in project with code : ${code}  !`
+                }})
+            }
         }
 
         if(code == undefined ){
@@ -58,6 +65,11 @@ router.get("/projectInfo/:id", async (req, res, next) => {
         }
     }catch(err){
         console.log(err);
+        return res.json({
+            data : {
+                status : "Something went wrong !"
+            }
+        })
     }
 
 })
