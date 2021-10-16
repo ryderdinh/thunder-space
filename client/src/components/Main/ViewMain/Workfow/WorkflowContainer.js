546fa8ff55efbac575e9b781e5eea4b94df73816
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2021-10-16 22:16:42 +0700

    .update logo

---
+++ client/src/components/Main/ViewMain/Workfow/WorkflowContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WorkflowContainer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WorkflowContainer
--- client/src/components/Main/ViewMain/Workfow/WorkflowItem.js
+++ client/src/components/Main/ViewMain/Workfow/WorkflowItem.js
@@ -1,71 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import WorkflowIntruction from "./WorkflowIntruction";
-import { motion } from "framer-motion";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import WorkflowIntruction from './WorkflowIntruction';
+import { motion } from 'framer-motion';
 
 export default function WorkflowItem({ data, variants }) {
-  // console.log(variants.visible.transition);
-  const {
-    type,
-    projectCode,
-    issueId,
-    issueCode,
-    issueType,
-    issueName,
-    toIssue,
-  } = data;
-  switch (type) {
-    case "project":
-      return (
-        <motion.div
-          className="wf--item-box"
-          variants={variants}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-        >
-          <Link to={`/${toIssue}`} className="wf--item pointer">
-            <div className="col">
-              <WorkflowIntruction
-                projectCode={projectCode}
-                issueId={issueId}
-                issueCode={issueCode}
-              />
-              <div className="wf--item-content">
-                <p>{issueName}</p>
-              </div>
-            </div>
-            <div className="col">
-              <div className={`wf--item-type ${issueType}`}></div>
-            </div>
-          </Link>
-        </motion.div>
-      );
+	// console.log(variants.visible.transition);
+	const {
+		type,
+		projectCode,
+		issueId,
+		issueCode,
+		issueType,
+		issueName,
+		toIssue
+	} = data;
+	console.log(data);
+	switch (type) {
+		case 'project':
+			return (
+				<motion.div
+					className='wf--item-box'
+					variants={variants}
+					initial='initial'
+					animate='enter'
+					exit='exit'
+				>
+					<Link to={`/${toIssue}`} className='wf--item pointer'>
+						<div className='col'>
+							<WorkflowIntruction
+								projectCode={projectCode}
+								issueId={issueId}
+								issueCode={issueCode}
+							/>
+							<div className='wf--item-content'>
+								<p>{issueName}</p>
+							</div>
+						</div>
+						<div className='col'>
+							<div className={`wf--item-type ${issueType}`}></div>
+						</div>
+					</Link>
+				</motion.div>
+			);
 
-    default:
-      return (
-        <motion.div
-          className="wf--item pointer"
-          variants={variants}
-          initial="initial"
-          animate="enter"
-          exit="exit"
-        >
-          <div className="col">
-            <WorkflowIntruction
-              projectCode={projectCode}
-              issueId={issueId}
-              issueCode={issueCode}
-            />
-            <div className="wf--item-content">
-              <p>{issueName}</p>
-            </div>
-          </div>
-          p
-          <div className="col">
-            <div className={`wf--item-type ${issueType}`}></div>
-          </div>
-        </motion.div>
-      );
-  }
+		default:
+			return (
+				<motion.div
+					className='wf--item pointer'
+					variants={variants}
+					initial='initial'
+					animate='enter'
+					exit='exit'
+				>
+					<div className='col'>
+						<WorkflowIntruction
+							projectCode={projectCode}
+							issueId={issueId}
+							issueCode={issueCode}
+						/>
+						<div className='wf--item-content'>
+							<p>{issueName}</p>
+						</div>
+					</div>
+					p
+					<div className='col'>
+						<div className={`wf--item-type ${issueType}`}></div>
+					</div>
+				</motion.div>
+			);
+	}
 }
