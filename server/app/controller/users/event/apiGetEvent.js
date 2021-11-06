505f654771f1fbf8995a318031d14920cabbbee2
unknown <boypham1234567@gmail.com>
2021-11-06 17:14:40 +0700

    fix api event

--- server/app/controller/users/event/apiGetEvent.js
+++ server/app/controller/users/event/apiGetEvent.js
@@ -6,18 +6,16 @@ const { convert } = require("../../../utils/dateFormat")
 module.exports  = (req, res) => {
     Event.find({}, (err, event) =>{
         if(err) {
-            res.json({data : {status : "Cannot access event"}})
+            res.status(400).send({status : "Cannot access event"})
         }
          event = event.map(e => 
             e = {
-                eid : e.eid,
+                eid : e._id,
                 name : e.name,
                 date : convert(e.date),
                 event_detail : e.event_detail
             }
         )
-        res.json({ 
-            event
-         })
+        res.status(200).send(event)
     })
 }
