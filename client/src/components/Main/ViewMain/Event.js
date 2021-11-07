af6f5bd816174543757bc30c702a2712db684563
dinhquanganh <dinhquanganhdev@gmail.com>
2021-11-07 20:41:10 +0700

    client@fix-event

--- client/src/components/Main/ViewMain/Event.js
+++ client/src/components/Main/ViewMain/Event.js
@@ -1,30 +1,31 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import EventItem from "./EventItem";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import EventItem from './EventItem';
 // import { actFetchEvents } from "actions";
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 export class Event extends Component {
-  render() {
-    const { events, variants } = this.props;
-    return (
-      <motion.div
-        className="view_item event"
-        variants={variants}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-      >
-        {events.map((value) => (
-          <EventItem key={value[0].date} dataEvent={value} />
-        ))}
-      </motion.div>
-    );
-  }
+	render() {
+		const { events, variants } = this.props;
+		console.log(events);
+		return (
+			<motion.div
+				className='view_item event'
+				variants={variants}
+				initial='hidden'
+				animate='visible'
+				exit='exit'
+			>
+				{events.map(value => (
+					<EventItem key={value[0].date} dataEvent={value} />
+				))}
+			</motion.div>
+		);
+	}
 }
 
-const mapStateToProps = (state) => ({
-  events: state._events._events,
+const mapStateToProps = state => ({
+	events: state._events._events
 });
 
 export default connect(mapStateToProps, null)(Event);
--- client/src/reducers/events.js
+++ client/src/reducers/events.js
@@ -1,29 +1,29 @@
 const initState = {
-  _events: [],
+	_events: []
 };
 
 export default function events(state = initState, action) {
-  switch (action.type) {
-    case "SET_EVENTS":
-      let events = action.payload.event;
-      let listEvent = [];
-      if (events !== undefined) {
-        let i = 0;
-        while (i <= events.length - 1) {
-          let arrR = [events[i]];
-          let j = i + 1;
-          while (j < events.length) {
-            if (events[i].date === events[j].date) {
-              arrR = [...arrR, events[j]];
-              events.splice(j, 1);
-            } else ++j;
-          }
-          listEvent = [...listEvent, arrR];
-          ++i;
-        }
-      }
-      return { ...state, _events: listEvent };
-    default:
-      return { ...state };
-  }
+	switch (action.type) {
+		case 'SET_EVENTS':
+			let events = action.payload;
+			let listEvent = [];
+			if (events) {
+				let i = 0;
+				while (i <= events.length - 1) {
+					let arrR = [events[i]];
+					let j = i + 1;
+					while (j < events.length) {
+						if (events[i].date === events[j].date) {
+							arrR = [...arrR, events[j]];
+							events.splice(j, 1);
+						} else ++j;
+					}
+					listEvent = [...listEvent, arrR];
+					++i;
+				}
+			}
+			return { ...state, _events: listEvent };
+		default:
+			return { ...state };
+	}
 }
