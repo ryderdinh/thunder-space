1ed43d25f725b4bc4fb11e5219cd4bf5f3858271
unknown <you@example.com>
2022-07-16 15:03:17 +0700

    hot fix

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const { server } = require('./src/app');
-const io = require('../server/src/io');
 //———————————————————————————APP LISTEN—————————————————————————————//
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
