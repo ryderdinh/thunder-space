d9b78a1cb1c819188bb97920a1545a7c74f14cf0
ryderdinh <dinhquanganhdev@gmail.com>
2023-02-04 02:55:43 +0700

    client@update timesheet

--- client/src/features/timesheets/index.jsx
+++ client/src/features/timesheets/index.jsx
@@ -1,15 +1,15 @@
-import { actFetchTimesheets } from 'actions'
-import { Motion } from 'components/Layouts'
-import CheckinFeature from 'features/checkin'
-import { getCheckinHour } from 'features/checkin/services/getCheckinHour'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import TimesheetsHeader from './components/TimesheetsHeader'
 import TimesheetsTable from './components/TimesheetsTable'
 import { getDayEnoughTime } from './services/getDayEnoughTime'
 import { getDayListByMonth } from './services/getDayListByMonth'
 import { getHistoryFromDate } from './services/getHistoryFromDate'
 import { handleMonths } from './services/handleDate'
+import { actFetchTimesheets } from 'actions'
+import { Motion } from 'components/Layouts'
+import CheckinFeature from 'features/checkin'
+import { getCheckinHour } from 'features/checkin/services/getCheckinHour'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const now = () => {
   let date = new Date()
@@ -35,7 +35,7 @@ const TimesheetsFeature = ({ variants }) => {
   const previous = () => {
     setTableTitle(handleMonths(-1, new Date(tableTitle)))
 
-    setMonth(month - 1)
+    setMonth((prev) => (prev === 1 ? 12 : prev - 1))
   }
 
   const returnPresent = () => {
@@ -48,7 +48,7 @@ const TimesheetsFeature = ({ variants }) => {
   const next = () => {
     setTableTitle(handleMonths(1, new Date(tableTitle)))
 
-    setMonth(month + 1)
+    setMonth((prev) => (prev === 12 ? 1 : prev + 1))
   }
 
   const getHistoryData = useCallback(
@@ -77,9 +77,13 @@ const TimesheetsFeature = ({ variants }) => {
 
       <TimesheetsTable
         setDay={setDay}
+        year={new Date(tableTitle).getFullYear()}
         month={month}
         day={day}
-        days={getDayListByMonth(month)}
+        days={getDayListByMonth(
+          Number(month),
+          new Date(tableTitle).getFullYear()
+        )}
         daysComplete={getDaysComplete}
       />
 
--- client/src/features/timesheets/services/getDayListByMonth.js
+++ client/src/features/timesheets/services/getDayListByMonth.js
@@ -1,4 +1,4 @@
-export const getDayListByMonth = (month) => {
+export const getDayListByMonth = (month, year) => {
   let dayInMonth = 0
   switch (month) {
     case 1:
@@ -7,23 +7,23 @@ export const getDayListByMonth = (month) => {
     case 7:
     case 8:
     case 10:
-    case 12:
+    case 12: {
       dayInMonth = 31
       break
-    case 2:
+    }
+    case 2: {
       if (
-        (dayInMonth % 4 === 0 &&
-          dayInMonth % 100 !== 0 &&
-          dayInMonth % 400 !== 0) ||
-        (dayInMonth % 100 === 0 && dayInMonth % 400 === 0)
+        (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) ||
+        (year % 100 === 0 && year % 400 === 0)
       )
         dayInMonth = 29
       else dayInMonth = 28
-
       break
-    default:
+    }
+    default: {
       dayInMonth = 30
       break
+    }
   }
 
   const positionDayStart = new Date(2022, month - 1, 1).getDay()
